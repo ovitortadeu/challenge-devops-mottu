eb26756b0272cf227e5e090afdb50cffa693d12d
Vitor Tadeu <140114555+ovitortadeu@users.noreply.github.com>
2025-09-28 18:27:33 -0300

    Update DemoApplication.java

--- challenge java/demo/src/main/java/br/com/challenge_java/DemoApplication.java
+++ challenge java/demo/src/main/java/br/com/challenge_java/DemoApplication.java
@@ -1,11 +1,8 @@
 package br.com.challenge_java;
 
-import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
 
 @EnableCaching
 @SpringBootApplication
@@ -15,11 +12,4 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
-	@Bean
-	public FlywayMigrationStrategy flywayMigrationStrategy() {
-		return flyway -> {
-			flyway.repair();
-			flyway.migrate();
-		};
-	}
 }
